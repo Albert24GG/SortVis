ede3bdb3525159d7610cad48ac862ce7943b518d
ImperiaL-24 <53470229+ImperiaL-24@users.noreply.github.com>
2021-08-01 20:07:50 +0300

    KOMET 5
    FȚ-18 in niciun caz!

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const button_right_count = document.getElementById("button-right-count");
+const button_left_count = document.getElementById("button-left-count");
+const button_right_speed = document.getElementById("button-right-speed");
+const button_left_speed  = document.getElementById("button-left-speed");
+
+const count = document.getElementById("count");
+const speed = document.getElementById("speed");
+
+count.addEventListener("input",() => {
+    if(count.value>999) count.value=999;
+    if(count.value<0) count.value=0;
+}); 
+
+speed.addEventListener("input",() => {
+    if(speed.value>999) speed.value=999;
+    if(speed.value<0) speed.value=0;
+
+}); 
+
+button_right_count.addEventListener("click",() => {
+    if(count.value<999) count.value++;
+}); 
+
+button_left_count.addEventListener("click",() => {
+    if(count.value>0) count.value--;
+});
+
+button_right_speed.addEventListener("click",() => {
+    if(speed.value<999) speed.value++;
+});
+
+button_left_speed.addEventListener("click",() => {
+    if(speed.value>0) speed.value--;
+});
