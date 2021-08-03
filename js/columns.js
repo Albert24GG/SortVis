e6fc06d78ed74c810b341fa818d28c225f7770fa
ImperiaL-24 <53470229+ImperiaL-24@users.noreply.github.com>
2021-08-03 23:26:21 +0300

    Swap Function

--- js/columns.js
+++ js/columns.js
@@ -1,49 +1,67 @@
 const canvas = document.getElementById("columns");
 const ctx = canvas.getContext("2d");
 let arr = [];
-var pos = [];
-let width = 40;
-let margin = 25;
+let pos = [];
+let rect_width;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight-70;
 
 function build(len){
-    
     for(let i=0;i<len;i++){
-        pos[i]=i*(width + margin);
+        pos[i]=i;
         arr[i]=i+1;
     }
 }
 
-build(5);
+build(20);
 
-let fps = 60;
+let fps = 48;
 let dtime = 1000/fps;
-let animdur = 750;
+let animdur = 5000;
+
 setInterval(() => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight-70;
-    let off =  (canvas.width - arr.length * (width+margin))/2;
+
+    rect_width = Math.min(canvas.width / ( 2 * arr.length),40);
+
+    let off =  (canvas.width - arr.length * (3*rect_width/2))/2;
     let unit = canvas.height / Math.max.apply(null, arr) * 3 / 4 ;
+
     for (let i = 0; i < arr.length; i++) {
         const h = arr[i];
         ctx.fillStyle = "#4ae2a5";
-        ctx.roundRect(off+pos[i],(canvas.height * 3 / 4)+20,width,-h*unit,5);
+        radius = Math.min(rect_width/2,5);
+        ctx.roundRect 
+        (
+            off+pos[i]*(3*rect_width/2),
+            (canvas.height * 3 / 4) + 20 ,
+            rect_width,
+            -h*unit,
+            radius
+        );
     }
     ctx.fillStyle = "#4ae2a5";
 },dtime);
 
-//lerp(loc : int -> indexul arrayului xoff in care va anima valoarea
-//     from: int -> valoarea de inceput
-//     to  : int -> valoarea de final
-//     time: float -> nu te intereseaza, e pt recursivitate
 
-
-let lerp = function (loc, from, to, time=0){
+let swap = function (from, to, time=0){
     let smooth = (1-Math.cos(time/1000*Math.PI))/2;
-    if(time<1000) {
-        setTimeout(function() {lerp(loc, from, to, time + dtime/(animdur/1000))},dtime);
+    if(time>1000) {
+        pos[from]=from;
+        pos[to]=to;
+
+        let aux = arr[from];
+        arr[from]=arr[to];
+        arr[to]=aux;
+        return;
     }
-    pos[loc] = from*(1-smooth)+to*smooth;
+    setTimeout(function() {swap(from, to, time + dtime/(animdur/1000))},dtime);
+    pos[from] = from*(1-smooth)+to*smooth;
+    pos[to] = to*(1-smooth)+from*smooth;
 }
+
 
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, width, height, radius=0) {
       ctx.beginPath();
@@ -59,6 +77,7 @@ CanvasRenderingContext2D.prototype.roundRect = function (x, y, width, height, ra
       ctx.closePath();
       ctx.fill();   
     }
+
 
 
   
