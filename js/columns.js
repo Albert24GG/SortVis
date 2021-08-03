28bf53f0a4bd642ac581bbae49bd35d6a97845f3
ImperiaL-24 <53470229+ImperiaL-24@users.noreply.github.com>
2021-08-03 21:19:52 +0300

    added roundRect

--- js/columns.js
+++ js/columns.js
@@ -1,9 +1,19 @@
 const canvas = document.getElementById("columns");
 const ctx = canvas.getContext("2d");
-let arr = [1,2,2,12,7];
-var xoff = [10, 0, 0,0,0,0]
+let arr = [];
+var pos = [];
 let width = 40;
 let margin = 25;
+
+function build(len){
+    
+    for(let i=0;i<len;i++){
+        pos[i]=i*(width + margin);
+        arr[i]=i+1;
+    }
+}
+
+build(5);
 
 let fps = 60;
 let dtime = 1000/fps;
@@ -16,9 +26,9 @@ setInterval(() => {
     for (let i = 0; i < arr.length; i++) {
         const h = arr[i];
         ctx.fillStyle = "#4ae2a5";
-        ctx.fillRect(off+i*(width + margin)+xoff[i],(canvas.height * 3 / 4)+20,width,-h*unit);
+        ctx.roundRect(off+pos[i],(canvas.height * 3 / 4)+20,width,-h*unit,5);
     }
-    
+    ctx.fillStyle = "#4ae2a5";
 },dtime);
 
 //lerp(loc : int -> indexul arrayului xoff in care va anima valoarea
@@ -32,5 +42,23 @@ let lerp = function (loc, from, to, time=0){
     if(time<1000) {
         setTimeout(function() {lerp(loc, from, to, time + dtime/(animdur/1000))},dtime);
     }
-    xoff[loc] = from*(1-smooth)+to*smooth;
+    pos[loc] = from*(1-smooth)+to*smooth;
 }
+
+CanvasRenderingContext2D.prototype.roundRect = function (x, y, width, height, radius=0) {
+      ctx.beginPath();
+      ctx.moveTo(x + radius, y);
+      ctx.lineTo(x + width - radius, y);
+      ctx.quadraticCurveTo(x + width, y, x + width, y - radius);
+      ctx.lineTo(x + width , y + height + radius);
+      ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+      ctx.lineTo(x + radius, y + height);
+      ctx.quadraticCurveTo(x, y + height, x, y + height + radius);
+      ctx.lineTo(x, y - radius);
+      ctx.quadraticCurveTo(x, y, x + radius, y);
+      ctx.closePath();
+      ctx.fill();   
+    }
+
+
+  
