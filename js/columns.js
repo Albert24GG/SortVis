52c509cd879ebebf3b50356a4e7cbd99ee8cfa3b
Albert24GG <guiman.albert09@gmail.com>
2021-08-04 17:48:19 +0300

    Add buttons functionality

--- js/columns.js
+++ js/columns.js
@@ -2,15 +2,23 @@ const canvas = document.getElementById("columns");
 const ctx = canvas.getContext("2d");
 let arr = [];
 let pos = [];
+let color = [];
 let rect_width;
+let is_started = false;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight-70;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
 
 function build(len){
     for(let i=0;i<len;i++){
         pos[i]=i;
         arr[i]=i+1;
+        color[i] = "#4ae2a5";
     }
 }
 
@@ -18,7 +26,7 @@ build(20);
 
 let fps = 48;
 let dtime = 1000/fps;
-let animdur = 5000;
+let animdur = 500;
 
 setInterval(() => {
     canvas.width = window.innerWidth;
@@ -29,9 +37,12 @@ setInterval(() => {
     let off =  (canvas.width - arr.length * (3*rect_width/2))/2;
     let unit = canvas.height / Math.max.apply(null, arr) * 3 / 4 ;
 
+
+
     for (let i = 0; i < arr.length; i++) {
         const h = arr[i];
-        ctx.fillStyle = "#4ae2a5";
+        //ctx.fillStyle = "#4ae2a5";
+        ctx.fillStyle = color[i];
         radius = Math.min(rect_width/2,5);
         ctx.roundRect 
         (
@@ -42,26 +53,97 @@ setInterval(() => {
             radius
         );
     }
-    ctx.fillStyle = "#4ae2a5";
+    //ctx.fillStyle = "#4ae2a5";
 },dtime);
-
 
 let swap = function (from, to, time=0){
     let smooth = (1-Math.cos(time/1000*Math.PI))/2;
-    if(time>1000) {
+    if(time>1000 || animdur < dtime) {
         pos[from]=from;
         pos[to]=to;
 
         let aux = arr[from];
         arr[from]=arr[to];
         arr[to]=aux;
+
         return;
     }
-    setTimeout(function() {swap(from, to, time + dtime/(animdur/1000))},dtime);
     pos[from] = from*(1-smooth)+to*smooth;
     pos[to] = to*(1-smooth)+from*smooth;
+    setTimeout(function() {swap(from, to, time + dtime/(animdur/1000))},dtime);
+
 }
 
+function randomize(){
+    if(is_started) return;
+    for(let i=0;i<arr.length;++i){
+        let rand = getRandomInt(arr.length);
+        let aux = arr[i];
+        arr[i] = arr[rand];
+        arr[rand] = aux;
+    }
+}
+
+let rebuild = function () {
+    if(is_started) return;
+    arr=[];
+    pos=[];
+    color=[];
+    build(count.value);
+}
+
+let addElem = function () {
+    if(is_started) return;
+    arr.push(arr.length+1);
+    pos.push(pos.length);
+}
+
+let delElem = function () {
+    if(is_started) return;
+    arr.pop();
+    pos.pop();
+}
+
+function sort(){
+    animdur = 500 / (parseInt(speed.value) / 100);
+    changeState(true);
+    if(alg.value == 1){
+        bubbleSort(arr);
+    }
+    
+}
+async function bubbleSort(arr){
+    var len = arr.length;
+    for (var i = 0; i<len; i++){
+      for(var j = 0; j<len-i-1; j++){
+        if(!is_started) return;
+        console.log(j);
+        if(arr[j]>arr[j+1]){
+            
+            //color[j] = color[j+1] = "#21c6e8";
+            //swap(j,j+1);
+            let aux = arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=aux;
+            await sleep(animdur+1);
+            //color[j] = color[j+1] = "#4ae2a5";
+            console.log(j);
+        }
+      }
+    }
+    changeState(false);
+ }
+
+let changeState = function (state=false) {
+    is_started = state;
+    count.disabled = state;
+    speed.disabled = state;
+    button_right_count.disabled = state;
+    button_left_count.disabled = state;
+    button_right_speed.disabled = state;
+    button_left_speed.disabled = state;
+    alg.disabled = state;
+}
 
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, width, height, radius=0) {
       ctx.beginPath();
@@ -77,6 +159,10 @@ CanvasRenderingContext2D.prototype.roundRect = function (x, y, width, height, ra
       ctx.closePath();
       ctx.fill();   
     }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
 
 
 
--- js/index.js
+++ js/index.js
@@ -2,34 +2,47 @@ const button_right_count = document.getElementById("button-right-count");
 const button_left_count = document.getElementById("button-left-count");
 const button_right_speed = document.getElementById("button-right-speed");
 const button_left_speed  = document.getElementById("button-left-speed");
+const alg = document.getElementById("sorting-alg");
 
 const count = document.getElementById("count");
 const speed = document.getElementById("speed");
 
-count.addEventListener("input",() => {
+
+count.addEventListener("input",(e) => {
+    //if(is_started) e.preventDefault();
     if(count.value>999) count.value=999;
     if(count.value<0) count.value=0;
+    rebuild();
 }); 
 
 speed.addEventListener("input",() => {
-    if(speed.value>999) speed.value=999;
+    if(speed.value>9999) speed.value=9999;
     if(speed.value<0) speed.value=0;
 
 }); 
 
 button_right_count.addEventListener("click",() => {
-    if(count.value<999) count.value++;
-    test();
+    if(is_started) return;
+    if(count.value>=999) return;
+    count.value++;
+    addElem();
 }); 
 
 button_left_count.addEventListener("click",() => {
-    if(count.value>0) count.value--;
+    if(is_started) return;
+    if(count.value<=0) return;
+    count.value--;
+    delElem();
 });
 
 button_right_speed.addEventListener("click",() => {
-    if(speed.value<999) speed.value++;
+    if(is_started) return;
+    if(speed.value> 9999) return; 
+    speed.value=parseInt(speed.value)+50;
 });
 
 button_left_speed.addEventListener("click",() => {
-    if(speed.value>0) speed.value--;
+    if(is_started) return;
+    if(speed.value <=0) return; 
+    speed.value=parseInt(speed.value)-50;
 });
